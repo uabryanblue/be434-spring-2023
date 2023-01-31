6079978c71212e1af5ac56ff12056327f344eaf0
uabryanblue <bryanblue@arizona.edu>
2023-01-31 15:35:17 -0700

    Final program didnt push other version

--- assignments/02_sum/sum.py
+++ assignments/02_sum/sum.py
@@ -2,7 +2,8 @@
 """
 Author : Bryan Blue bryanblue@arizona.edu
 Date   : 2023-01-31
-Purpose: Assignment 2 BE534, Spring 2023
+Purpose: Sum integer values
+University of Arizona, BE534
 """
 
 import argparse
@@ -18,7 +19,7 @@ def get_args():
     )
 
     parser.add_argument(
-        "-i", "--int", help="Numbers to add", metavar="INT", type=int, default=0
+        "INT", nargs="+", help="Numbers to add", type=int, metavar="INT"
     )
 
     return parser.parse_args()
@@ -29,9 +30,16 @@ def main():
     """Add numbers together"""
 
     args = get_args()
-    int_arg = args.int
+    # add_argument statement takes care of type checking
+    # list of integer values to be added together
+    int_args = args.INT
 
-    print(f'int_arg = "{int_arg}"')
+    # pg 400 from Tiny Python Projects
+    # advanced for this chapter, but the videos encourges
+    # you to review the documentation in the book (old formatting)
+    # print("{} = {}".format(" + ".join(map(str, int_args)), sum(int_args)))
+    # this is converted to the f string format
+    print(f'{" + ".join(map(str, int_args))} = {sum(int_args)}')
 
 
 # --------------------------------------------------
