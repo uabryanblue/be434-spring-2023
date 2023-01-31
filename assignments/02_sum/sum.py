1e85d72bfe3a17609eac92543378162cf8616e73
uabryanblue <bryanblue@arizona.edu>
2023-01-31 11:31:12 -0700

    Outline in place, needs coded

--- assignments/02_sum/sum.py
+++ assignments/02_sum/sum.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
-Author : lazuline <lazuline@localhost>
+Author : Bryan Blue bryanblue@arizona.edu
 Date   : 2023-01-31
-Purpose: Rock the Casbah
+Purpose: Assignment 2 BE534, Spring 2023
 """
 
 import argparse
@@ -10,63 +10,30 @@ import argparse
 
 # --------------------------------------------------
 def get_args():
-    """Get command-line arguments"""
+    """Add numbers"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Add numbers",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument(
+        "-i", "--int", help="Numbers to add", metavar="INT", type=int, default=0
+    )
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Add numbers together"""
 
     args = get_args()
-    str_arg = args.arg
     int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
     print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
