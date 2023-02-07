9ac7b171362ed99e5e0226cea49f123069cbdf0b
uabryanblue <bryanblue@arizona.edu>
2023-02-07 07:43:56 -0700

    created dictionary and capture input

--- assignments/03_solfege/solfege.py
+++ assignments/03_solfege/solfege.py
@@ -2,7 +2,7 @@
 """
 Author : lazuline <lazuline@localhost>
 Date   : 2023-02-05
-Purpose: Rock the Casbah
+Purpose: Assignment 03, BE534
 """
 
 import argparse
@@ -13,60 +13,37 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Rock the Casbah",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument("str", metavar="str", nargs="+", help="Solfege")
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Dictionary Assignment"""
 
+    # parse command line
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    str_arg = args.str
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    # configure solfege dictionary
+    syllables = dict(
+        Do="A deer, a female deer",
+        Re="A drop of golden sun",
+        Mi="A name I call myself",
+        Fa="A long long way to run",
+        Sol="A needle pulling thread",
+        La="A note to follow sol",
+        Ti="A drink with jam and bread",
+    )
+
+    print(f'syllables = "{syllables}"')
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
