6d6cf2dbeaaf52c641568c5ec83fab7ff26b92a2
uabryanblue <bryanblue@arizona.edu>
2023-02-21 08:45:22 -0700

    Pass first 6 tests argsparse

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -6,45 +6,36 @@ Purpose: exercise 5 - translate.py
 """
 
 import argparse
-
+import sys
 
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='exercise 5 - translate.py',
+        description='Translate DNA/RNA to proteins',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
+    parser.add_argument('-c',
+                        '--codons',
+                        required=True,
+                        help='A file with codon translations',
                         metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
+                        type=argparse.FileType('rt'))
+                        #default=sys.stdin)
 
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--output',
+                        help='A readable file',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'))
+                        # default='out.txt')
+
+    parser.add_argument("str",
+                        metavar="str",
+                        help="DNA/RNA sequence")
+
 
     return parser.parse_args()
 
@@ -54,17 +45,7 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
 
 
 # --------------------------------------------------
