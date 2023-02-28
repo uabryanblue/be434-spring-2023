0293e84c659d2178a7ee2e434bc09172f4002b97
uabryanblue <bryanblue@arizona.edu>
2023-02-28 09:04:28 -0700

    args created

---
+++ assignments/06_iupac/iupac.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+Author : Homework 06 <bryanblue@arizona.edu>
+Date   : 2023-02-28
+Purpose: Homework 06
+"""
+
+import argparse
+import re
+import sys
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Expand IUPAC codes',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('SEQ',
+                        metavar='SEQ',
+                        nargs="+",
+                        help='Input sequence(s)')
+
+    parser.add_argument('-o',
+                        '--output',
+                        help='Output filename',
+                        metavar='FILE',
+                        default=sys.stdout,
+                        type=argparse.FileType("wt"),
+                        )
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    SEQ = args.SEQ
+    FILE = args.output
+
+    print(f'str_arg = "{SEQ}"')
+    print('file_arg = "{}"'.format(FILE if FILE else ''))
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
