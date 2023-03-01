36cd6461b46ba8a86553c9dd529ceb827e717c00
uabryanblue <bryanblue@arizona.edu>
2023-03-01 13:08:44 -0700

    added more comments

--- assignments/quiz6.py
+++ assignments/quiz6.py
@@ -22,11 +22,14 @@ print(f'original: {new_text}')
 # Question answer options:
 
 ###############
-# "map" solution has a syntax error
+## "map" solution has a syntax error
+## even if new_char and args.text arche change to hard coded values
 # def new_char(char):
 #     return vowel if char in 'aeiou' else vowel.upper() if char in 'AEIOU' else char
-
-# print(''.join.(map(new_char, args.text))) # syntax error
+## ORIGINAL LINE - don't have args parameters, so create a new line with variable changes
+# print(''.join.(map(new_char, args.text))) # !! SYNTAX ERROR
+## MODIFIED LINE - remplaced new_char with vowel and args.text with text
+# print(''.join.(map(vowel, text))) # !! SYNTAX ERROR
 
 print(f'test that uses: "''.join.(map(new_char, args.text)))"  solution generates syntax error')
 
