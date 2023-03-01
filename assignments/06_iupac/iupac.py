a8d2d81818d5d1c5efceb65ef04f5dd94323f722
uabryanblue <bryanblue@arizona.edu>
2023-03-01 06:54:05 -0700

    starting to work with writing to file

--- assignments/06_iupac/iupac.py
+++ assignments/06_iupac/iupac.py
@@ -45,30 +45,31 @@ def main():
     # create dictionary of IUPAC -> Base code translations
     codes ={} 
     codes['A'] = 'A'
-    codes['C'] = 'C'    
-    codes['G'] = 'G'    
-    codes['T'] = 'T'    
-    codes['U'] = 'U'    
-    codes['R'] = 'AG'   
-    codes['Y'] = 'CT'   
-    codes['S'] = 'GC'   
-    codes['W'] = 'AT'   
-    codes['K'] = 'GT'   
-    codes['M'] = 'AC'   
-    codes['B'] = 'CGT'  
-    codes['D'] = 'AGT'  
-    codes['H'] = 'ACT'  
-    codes['V'] = 'ACG'  
-    codes['N'] = 'ACGT'  
+    codes['C'] = 'C'
+    codes['G'] = 'G'
+    codes['T'] = 'T'
+    codes['U'] = 'U'
+    codes['R'] = '[AG]'
+    codes['Y'] = '[CT]'
+    codes['S'] = '[GC]'
+    codes['W'] = '[AT]'
+    codes['K'] = '[GT]'
+    codes['M'] = '[AC]'
+    codes['B'] = '[CGT]'
+    codes['D'] = '[AGT]'
+    codes['H'] = '[ACT]'
+    codes['V'] = '[ACG]'
+    codes['N'] = '[ACGT]' 
 
     # print(f'str_arg = "{SEQ}"')
     # print('file_arg = "{}"'.format(FILE if FILE else ''))
-
+   
     for dna in SEQ:
-        text = ''
+        text = dna + ' '
         for dna_char in dna:
             text += codes.get(dna_char,dna_char) #, out_txt, c)        
-        print(f'{text}')       
+        FILE.write(text)       
+        FILE.write('\n')
 
 
 # --------------------------------------------------
