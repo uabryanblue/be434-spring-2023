5137de218557b0a8d444db8d93b1f82cab15ef66
uabryanblue <bryanblue@arizona.edu>
2023-04-01 17:34:39 -0700

    test 8 works for me, fails test.py - sent message

--- assignments/09_csvfilter/csvfilter.py
+++ assignments/09_csvfilter/csvfilter.py
@@ -6,10 +6,7 @@ Purpose: Rock the Casbah
 """
 
 import argparse
-import numpy as np
 import pandas as pd
-# import csv
-# import re
 import re
 import sys
 
@@ -18,47 +15,51 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Filter delimited records',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Filter delimited records",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="Input file",
+        metavar="FILE",
+        required=True,
+        # type=str)
+        type=argparse.FileType("rt"),
+    )
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='Input file',
-                        metavar='FILE',
-                        required=True,
-                        # type=str)
-                        type=argparse.FileType('rt'))
+    parser.add_argument(
+        "-v",
+        "--val",
+        help="Value for filter",
+        metavar="val",
+        required=True,
+        type=str,
+        default=None,
+    )
 
-    parser.add_argument('-v',
-                        '--val',
-                        help='Value for filter',
-                        metavar='val',
-                        required=True,
-                        type=str,
-                        default=None)
-    
-    parser.add_argument('-c',
-                        '--col',
-                        help='Column for filter',
-                        metavar='col',
-                        type=str,
-                        default='')
+    parser.add_argument(
+        "-c", "--col", help="Column for filter", metavar="col", type=str, default=""
+    )
 
-    parser.add_argument('-o',
-                        '--outfile',
-                        help='Output filename',
-                        metavar='OUTFILE',
-                        type=str,
-                        default='out.csv')
-    
-    parser.add_argument('-d',
-                        '--delimiter',
-                        help='Input delimiter',
-                        metavar='delim',
-                        type=str,
-                        default=',')
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Output filename",
+        metavar="OUTFILE",
+        type=str,
+        default="out.csv",
+    )
 
+    parser.add_argument(
+        "-d",
+        "--delimiter",
+        help="Input delimiter",
+        metavar="delim",
+        type=str,
+        default=",",
+    )
 
     return parser.parse_args()
 
@@ -66,54 +67,57 @@ def get_args():
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
-    
+
     args = get_args()
     # print(f'using delimeter:{args.delimiter}')
-    
+
     # read the input file in a data frame, make sure all fields are strings
-    df = pd.read_csv(args.file.name, sep=args.delimiter, dtype = str, engine='python')
-     
-    # if the col argument is specified, check that it is in the file column headers     
-    if(args.col and (args.col not in df.columns)):
+    df = pd.read_csv(args.file.name, sep=args.delimiter, dtype=str, engine="python")
+
+    # if the col argument is specified, check that it is in the file column headers
+    if args.col and (args.col not in df.columns):
         args.file.close()
         print(f'--col "{args.col}" not a valid column!')
-        mylist = ', '.join(df.columns.tolist())
-        sys.exit(f'Choose from {mylist}')
-    
+        mylist = ", ".join(df.columns.tolist())
+        sys.exit(f"Choose from {mylist}")
+
     # print(df.head())
     # print(f'Looking for:{args.val}')
     # print(f'looking in column:{args.col}')
     if args.col:
         # find all rows that match the input value in a given column, case insensitive
-        # outdf = df[df[args.col].str.lower() == args.val.lower()]
 
         # search for a substring of args.val in the field, ignoring case
-        pattern = re.compile('.*' + args.val + '.*', re.IGNORECASE)
+        pattern = re.compile(".*" + args.val + ".*", re.IGNORECASE)
         # print(pattern)
+
         # create a binary mask from the given column using the regex
-        mask = df[[args.col]].apply(
-            lambda x: x.str.contains(
-            pattern,
-            regex=True
-            )
-        ).any(axis=1)
+        mask = (
+            df[[args.col]]
+            .apply(lambda x: x.str.contains(pattern, regex=True))
+            .any(axis=1)
+        )
         # print(df[mask])
+
         # use the mask to create a dataframe of values
         outdf = df[mask]
     else:
         # find all all rows that match any column for a given value, case insensitive
         # print(f'no col specified')
-        outdf = df[df.apply(lambda row: row.str.contains(args.val, case=False).any(), axis=1)] 
+        outdf = df[
+            df.apply(lambda row: row.str.contains(args.val, case=False).any(), axis=1)
+        ]
 
     # no records found, report it
     if outdf.shape[0] == 0:
         args.file.close()
-        sys.exit(f'No usable data in --file "{args.file.name}"')    
+        sys.exit(f'No usable data in --file "{args.file.name}"')
     else:
         # write out same structure with the lines that matched
-        outdf.to_csv(args.outfile, header=True, index=False, mode='wt')
+        outdf.to_csv(args.outfile, header=True, index=False, mode="wt")
         print(f'Done, wrote {outdf.shape[0]} to "{args.outfile}".')
 
+
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
