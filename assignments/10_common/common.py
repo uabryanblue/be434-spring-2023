6813d524bf92f43868c594a26f91097f7df5724b
uabryanblue <bryanblue@arizona.edu>
2023-04-04 15:14:51 -0700

    works, not cleaned up

--- assignments/10_common/common.py
+++ assignments/10_common/common.py
@@ -6,7 +6,7 @@ Purpose: Find Common Words
 """
 
 import argparse
-
+import sys
 
 # --------------------------------------------------
 def get_args():
@@ -16,16 +16,13 @@ def get_args():
         description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
+    parser.add_argument('file1',
+                        metavar='FILE1',
+                        help='Input file 1')
+    
+    parser.add_argument('file2',
+                    metavar='FILE2',
+                    help='Input file 2')
 
     parser.add_argument('-i',
                         '--int',
@@ -34,17 +31,13 @@ def get_args():
                         type=int,
                         default=0)
 
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--outfile',
+                        help='Output file',
+                        metavar='str',
+                        type=argparse.FileType('wt'),
+                        default=sys.stdout)
+
 
     return parser.parse_args()
 
@@ -54,19 +47,47 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    ofh = args.outfile
+    
+    try:
+        fh1 = open(args.file1, 'rt')
+    except:
+        sys.exit(f"No such file or directory: '{args.file1}'")
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    try:
+        fh2 = open(args.file2, 'rt')
+    except:
+        sys.exit(f"No such file or directory: '{args.file2}'")
+
+# --------------------------------------------------
+    def read_words_into_set(fh):
+        """read all white space delimited text into a set from a file"""
+        MySet = set()
+        for line in fh:
+            for word in line.split():
+                MySet.add(word)
+        return MySet
+# --------------------------------------------------
+
+    set1 = set()
+    set2 = set()
+    
+    set1 = read_words_into_set(fh1)
+    set2 = read_words_into_set(fh2)
+            
+    # print(f'set1: {set1}')
+    # print(f'set2: {set2}')
+
+    output = '\n'.join(sorted(set1.intersection(set2)))
+    ofh.write(output)
+    ofh.write('\n') # put a trailing return in for clarity
+
+    fh1.close()
+    fh2.close()
+    ofh.close()
 
 
+        
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
