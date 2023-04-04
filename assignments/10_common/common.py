9c5215c537f44f84f7f8a85239c954f7d348359c
uabryanblue <bryanblue@arizona.edu>
2023-04-04 15:45:24 -0700

    final code for base solution

--- assignments/10_common/common.py
+++ assignments/10_common/common.py
@@ -8,86 +8,97 @@ Purpose: Find Common Words
 import argparse
 import sys
 
+
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Rock the Casbah",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('file1',
-                        metavar='FILE1',
-                        help='Input file 1')
-    
-    parser.add_argument('file2',
-                    metavar='FILE2',
-                    help='Input file 2')
+    parser.add_argument("file1", metavar="FILE1", help="Input file 1")
 
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
+    parser.add_argument("file2", metavar="FILE2", help="Input file 2")
 
-    parser.add_argument('-o',
-                        '--outfile',
-                        help='Output file',
-                        metavar='str',
-                        type=argparse.FileType('wt'),
-                        default=sys.stdout)
+    parser.add_argument(
+        "-i",
+        "--int",
+        help="A named integer argument",
+        metavar="int",
+        type=int,
+        default=0,
+    )
 
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Output file",
+        metavar="str",
+        type=argparse.FileType("wt"),
+        default=sys.stdout,
+    )
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """
+    use sets to output commone words between two input files
+
+    """
 
     args = get_args()
     ofh = args.outfile
-    
+
+    # try to open both required input files, if it fails,
+    # output error message and exit
     try:
-        fh1 = open(args.file1, 'rt')
-    except:
+        fh1 = open(args.file1, "rt", encoding="utf-8")
+    except IOError:
+        ofh.close()
         sys.exit(f"No such file or directory: '{args.file1}'")
 
     try:
-        fh2 = open(args.file2, 'rt')
-    except:
+        fh2 = open(args.file2, "rt", encoding="utf-8")
+    except IOError:
+        ofh.close()
         sys.exit(f"No such file or directory: '{args.file2}'")
 
-# --------------------------------------------------
+    # --------------------------------------------------
     def read_words_into_set(fh):
-        """read all white space delimited text into a set from a file"""
+        """read all white space delimited text
+        into a set from a file"""
         MySet = set()
         for line in fh:
             for word in line.split():
                 MySet.add(word)
         return MySet
-# --------------------------------------------------
 
+    # --------------------------------------------------
+    # initializations
     set1 = set()
     set2 = set()
-    
+
     set1 = read_words_into_set(fh1)
     set2 = read_words_into_set(fh2)
-            
+
     # print(f'set1: {set1}')
     # print(f'set2: {set2}')
 
-    output = '\n'.join(sorted(set1.intersection(set2)))
-    ofh.write(output)
-    ofh.write('\n') # put a trailing return in for clarity
+    # output needs to be a sorted, new line delimited
+    # intersection of the two sets
+    # out = sorted(set1.intersection(set2))
+    [ofh.write(f"{item}\n") for item in sorted(set1.intersection(set2))]
 
+    # close out all open file handles
     fh1.close()
     fh2.close()
     ofh.close()
 
 
-        
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
