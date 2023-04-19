56e22fa61d2c93e18becf08c32acfd7f22e51d2b
uabryanblue <bryanblue@arizona.edu>
2023-04-19 09:54:19 -0700

    final version

--- assignments/12_grep/grep.py
+++ assignments/12_grep/grep.py
@@ -1,63 +1,87 @@
 #!/usr/bin/env python
 """
-Author : lazuline <lazuline@localhost>
+Author : Bryan Blue bryanblue@arizona.edu
 Date   : 2023-04-19
-Purpose: Rock the Casbah
+Purpose: Pythong grepm
 """
 
 import argparse
 import os
+import re
 import sys
+
 
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Python grep",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('pattern',
-                        metavar='PATTERN',
-                        help='Search pattern')
-    
-    parser.add_argument('-i',
-                        '--insensitive',
-                        help='insensitive search',
-                        action='store_true',
-                        default=False)
-    
-    parser.add_argument('FILE',
-                        metavar='FILE',
-                        nargs='+',
-                        help='Input file(s)')
+    parser.add_argument("pattern", metavar="PATTERN", help="Search pattern")
 
-    parser.add_argument('-o',
-                        '--outfile',
-                        help='Input file(s)',
-                        metavar='FILE',
-                        type=argparse.FileType('wt'),
-                        default=sys.stdout)
+    parser.add_argument(
+        "-i",
+        "--insensitive",
+        help="insensitive search",
+        action="store_true",
+        default=False,
+    )
 
+    parser.add_argument(
+        "files", metavar="FILE", type=str, nargs="+", help="Input file(s)"
+    )
+
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Output file",
+        metavar="FILE",
+        type=argparse.FileType("wt"),
+        default=sys.stdout,
+    )
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
+def create_pattern(pattern, ignore_case):
+    """create a regular expression using the re module
+    which also handles case insensitivity"""
+
+    re_pattern = re.compile(pattern)
+    if ignore_case:
+        re_pattern = re.compile(pattern, re.IGNORECASE)
+    return re_pattern
+
+
+# --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Python grep"""
 
     args = get_args()
-    # ofh = args.outfile
+    # print(args)
 
-    # try to open required input files, if it fails,
-    # output error message and exit
-    for name in args.FILE:
+    # verify all passed in input files exist
+    for name in args.files:
         if not os.path.isfile(name):
             sys.exit(f"error: No such file or directory: '{name}'")
 
-    print("hi")
+    # create a clean regex pattern
+    re_pattern = create_pattern(args.pattern, args.insensitive)
+    # find all lines that match the pattern in all filenames
+    NumFiles = len(args.files)
+    for name in args.files:
+        with open(name, "rt", encoding="utf8") as fh:
+            for line in fh:
+                # if found, do not care about where, dump line
+                if re.search(re_pattern, line):
+                    out = f"{name}: {line}" if NumFiles > 1 else line
+                    args.outfile.write(out)
+
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
