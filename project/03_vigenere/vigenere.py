6e7aa4331e6ecd42d828a2b4fa1ee04ae7007fc6
uabryanblue <bryanblue@arizona.edu>
2023-04-23 12:22:14 -0700

    messed up, refactoring

--- project/03_vigenere/vigenere.py
+++ project/03_vigenere/vigenere.py
@@ -2,10 +2,11 @@
 """
 Author : Bryan Blue bryanblue@arizona.edu
 Date   : 2023-04-23
-Purpose: Rock the Casbah
+Purpose: Vigenere Ciphers
 """
 
 import argparse
+import string
 import sys
 
 # --------------------------------------------------
@@ -13,7 +14,7 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description="Rock the Casbah",
+        description="Vigenere Ciphers",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
@@ -51,10 +52,91 @@ def get_args():
 
 
 # --------------------------------------------------
+# def test_shift():
+#     """test the common cases of error"""
+
+#     assert map_char("T", 0, 'TEST') == 'M'
+#     assert map_char("H", 1, 'TEST') == 'L'
+#     assert map_char("E", 2, 'TEST') == 'W'
+#     assert map_char("Q", 3, 'TEST') == 'J'
+#     assert map_char("U", 4, 'TEST') == 'N'
+#     assert map_char("I", 5, 'TEST') == 'M'
+#     # assert map_char("S", 34, 'TEST') == 'G'  
+#     # assert map_char("G", 35, 'TEST') == 'Z'
+    
+#     # Don't worry, spiders,
+#     # FWC'A AFTZN, ZTZFMGZ,
+#     assert map_char("D", 0, 'TEST') == 'F'
+#     assert map_char("S", 13, 'TEST') == 'Z'  
+#     assert map_char(",", 20, 'TEST') == ','
+
+
+# --------------------------------------------------
+def get_alpha_number(inchar):
+    out = ord(inchar) - 65
+    return out
+    
+def alpha_test():
+    for i, c in enumerate(string.ascii_uppercase):
+        alphpos = get_alpha_number(c)
+        print(f"i:{i}; chr:{c}; alphabet pos; {alphpos}")
+    return
+# --------------------------------------------------
+def map_char(inchar, index, keyword):
+    """translate inchar with its index and keyword index
+        using base 26"""
+
+    # if the character is not in A-Z just return inchar
+    if inchar not in string.ascii_uppercase:
+        # print(f"FOUND NON ALPHA:{inchar}:")
+        return inchar
+
+    # the ordinal position of 'A' is 65 use this in calculations
+    # calculate the input character position in A-Z, 0 based
+    InValue = ord(inchar) - 65
+    # print(f"--inchar: {inchar}, Index: {index} InOrd: {InValue}")
+    
+    KeyLen = len(keyword)
+    HashIndex = index % KeyLen
+    # HashIndex = (index + 1) % KeyLen
+    # print(f"KeyLen: {KeyLen}, HashIndex: {HashIndex}")
+    
+    KeyValue = ord(keyword[HashIndex]) - 65
+    # print(f"keyvalue: {KeyValue} HashCar:{keyword[HashIndex]}")
+
+    FinalOrd = (InValue + KeyValue) % 26 + 65
+    # print(f"FinalOrd: {FinalOrd} finalchar: {chr(FinalOrd)}")
+
+    return chr(FinalOrd)
+
+
+# --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """Vigenere Ciphers"""
 
     args = get_args()
+
+    alpha_test()
+    exit()
+
+    for line in args.infile:
+
+        line = line.upper() # ''.join(line.upper().split())
+        print(line, args.keyword)
+        # print(str.upper(line))
+        # assert map_char("D", 0, 'TEST') == 'F'
+        # assert map_char("S", 13, 'TEST') == 'Z' 
+        for i,c in enumerate(line):
+            mapped = map_char(c.upper(), i, args.keyword)
+            # print("mapped")
+            # print(c, i, mapped, ord(c), ord(mapped))
+            args.outfile.write(mapped)
+            # print(c, i)
+        args.outfile.write('\n')
+        # print(line)
+        # for c in line:
+        # # args.outfile.write(''.join([shift_forward(c.upper(), Shift).upper()
+        #                             for c in line]))
 
 
 # --------------------------------------------------
